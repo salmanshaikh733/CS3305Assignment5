8823aed4214784f015cc59c4cf2977c3f1d76b65
Salman <salman.shaikh1678@gmail.com>
2021-11-29 23:33:50 -0500

    initial commit

---
+++ cmake-build-debug/CMakeFiles/assignment5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment5.dir/main.c.o"
+  "assignment5.pdb"
+  "assignment5"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assignment5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
